f7e14bfc04c2504196fa0fb900e9aa6649453360
hewins7762@mail.philau.edu <hewins7762@mail.philau.edu>
2017-03-01 20:14:50 -0500

    First commit

---
+++ js/graphic.js
@@ -0,0 +1,62 @@
+// CRISIS
+$(document).ready(function () {
+  $("#crisis").click(function () {
+    $("#crisisText").slideToggle();
+  });
+});
+
+// SWEETEST SUITE
+$(document).ready(function () {
+  $("#sweet").click(function () {
+    $("#sweetText").slideToggle();
+  });
+});
+
+// SEX DEGREES
+$(document).ready(function () {
+  $("#sexDegrees").click(function () {
+    $("#sexDegreesText").slideToggle();
+  });
+});
+
+// LAUNDRY
+$(document).ready(function () {
+  $("#laundry").click(function () {
+    $("#laundryText").slideToggle();
+  });
+});
+
+// FACTS
+$(document).ready(function () {
+  $("#facts").click(function () {
+    $("#factsText").slideToggle();
+  });
+});
+
+// RA
+$(document).ready(function () {
+  $("#ra").click(function () {
+    $("#raText").slideToggle();
+  });
+});
+
+// FRANKLIN
+$(document).ready(function () {
+  $("#franklin").click(function () {
+    $("#franklinText").slideToggle();
+  });
+});
+
+// ORL
+$(document).ready(function () {
+  $("#orl").click(function () {
+    $("#orlText").slideToggle();
+  });
+});
+
+// OUTDOORS
+$(document).ready(function () {
+  $("#outdoors").click(function () {
+    $("#outdoorsText").slideToggle();
+  });
+});
